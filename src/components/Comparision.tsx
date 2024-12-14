8ae12f13933ca16802b667fcb801af193279eb93
AB Rizvi <abrizvi@mac.home>
2024-12-13 22:32:41 -0500

    Build issues

--- src/components/Comparision.tsx
+++ src/components/Comparision.tsx
@@ -1,7 +1,6 @@
 import ComparisionCard from "./ComparisionCard"
 import { appleIntelligenceIcon, a18Icon, cameraControlIcon, 
          baseCameraIcon, proCameraIcon, batteryIcon, a18ProIcon,  } from "../utils/image"
-import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
 
