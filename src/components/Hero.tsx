63248b6a701425029b39c2bc87553a73a84b74dc
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2025-03-17 22:41:38 -0400

    Added lazy loading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import { hero_imgs } from "../utils/image"
 import ResponsiveImage from "./ResponsiveImage"
 import gsap from 'gsap'
--- src/components/ResponsiveImage.tsx
+++ src/components/ResponsiveImage.tsx
@@ -1,18 +1,34 @@
+import { useState } from "react"
 import { ImageData } from "../utils/interfaces"
 
 interface Props {
     imageData: ImageData
 }
 
-function ResponsiveImage({imageData}: Props) {
+function Loading() {
     return (
-        <picture>
-            <source media="(max-width: 767px)" srcSet={imageData.small} />
-            <source media="(min-width: 768px) and (max-width: 1024px)" srcSet={imageData.medium} />
-            <source media="(min-width: 1025px) and (max-width: 1280px)" srcSet={imageData.large} />
-            <source media="(min-width: 1281px)" srcSet={imageData.xLarge} />
-            <img className="img" src={imageData.medium} alt="Responsive" />
-        </picture>
+        <h1>Loading...</h1>
+    )
+}
+
+function ResponsiveImage({imageData}: Props) {
+    const [isLoading, setIsLoading] = useState(true)
+    return (
+        <>
+            {isLoading && <Loading/>}
+            <picture>
+                <source media="(max-width: 767px)" 
+                        srcSet={imageData.small} />
+                <source media="(min-width: 768px) and (max-width: 1024px)" 
+                        srcSet={imageData.medium} />
+                <source media="(min-width: 1025px) and (max-width: 1280px)" 
+                        srcSet={imageData.large}/>
+                <source media="(min-width: 1281px)" 
+                        srcSet={imageData.xLarge}/>
+                <img className="img" src={imageData.medium} 
+                    alt="Responsive" onLoad={() => setIsLoading(false)}/>
+            </picture>
+        </>
     )
 }
 
